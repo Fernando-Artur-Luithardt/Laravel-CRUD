00d56eb163664296773826a04025d78bcdb52228
Fernando Artur Luithardt <91349414+Fernando-Artur-Luithardt@users.noreply.github.com>
2023-07-29 12:51:22 -0300

    produto->estoque OK!

--- app/Http/Controllers/EstoqueController.php
+++ app/Http/Controllers/EstoqueController.php
@@ -8,9 +8,7 @@ use App\Models\Produto;
 class EstoqueController extends Controller
 {
     public function estoque(){
-        $estoque = Produto::all();
-        dd($estoque);
-        exit;
-        return view('estoque');
+        $estoque = Produto::select(['produto.nome','produto.sku', Produto::raw('sum(controleEstoque.quantidade) as qtdEstoque')])->join('produtotipo', 'produtotipo.id', '=', 'produto.tipo')->leftJoin('controleEstoque', 'controleEstoque.produtoSku', '=', 'produto.sku')->groupBy('produto.sku')->get();
+        return view('estoque', ['estoque' => $estoque]);
     }
 }
--- resources/views/estoque.blade.php
+++ resources/views/estoque.blade.php
@@ -1,3 +1,17 @@
 <x-app-layout>
-    
+    <table>
+        <tr>
+            <th>SKU</th>
+            <th>Nome</th>
+            <th>Quantidade</th>
+        </tr>
+
+        @foreach ($estoque as $item)
+            <tr>
+                <th> {{ $item->sku }}</th>
+                <th> {{ $item->nome }}</th>
+                <th> {{ $item->qtdEstoque ? $item->qtdEstoque : 0 }}</th>
+            </tr>
+        @endforeach
+    </table>
 </x-app-layout>
